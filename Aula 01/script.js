780dc93f8ef2dbf03c317ebf0728203a871bfd48
Isabella Melo <isacpmelo@gmail.com>
2022-04-05 22:11:19 -0300

    Adição da Aula 01

---
+++ Aula 01/script.js
@@ -0,0 +1,11 @@
+// Scripts Calculadora de Média
+var notaDoPrimeiroBimestre = 9.45
+var notaDoSegundoBimestre = 7.24
+var notaTerceiroBimestre = 4.32
+var notaQuartoBimestre = 2.52
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(2)
+
+console.log(notaFixada)
